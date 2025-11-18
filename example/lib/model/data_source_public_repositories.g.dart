ea395cb6f8809df2c2fcff8839ceb4c958adae3b
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-11-18 20:43:37 +0900

    chore: Update packages

--- example/lib/model/data_source_public_repositories.g.dart
+++ example/lib/model/data_source_public_repositories.g.dart
@@ -6,25 +6,54 @@ part of 'data_source_public_repositories.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(dataSourcePublicRepositories)
+const dataSourcePublicRepositoriesProvider =
+    DataSourcePublicRepositoriesProvider._();
+
+final class DataSourcePublicRepositoriesProvider
+    extends
+        $FunctionalProvider<
+          DataSourcePublicRepositories,
+          DataSourcePublicRepositories,
+          DataSourcePublicRepositories
+        >
+    with $Provider<DataSourcePublicRepositories> {
+  const DataSourcePublicRepositoriesProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'dataSourcePublicRepositoriesProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$dataSourcePublicRepositoriesHash();
+
+  @$internal
+  @override
+  $ProviderElement<DataSourcePublicRepositories> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  DataSourcePublicRepositories create(Ref ref) {
+    return dataSourcePublicRepositories(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(DataSourcePublicRepositories value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<DataSourcePublicRepositories>(value),
+    );
+  }
+}
+
 String _$dataSourcePublicRepositoriesHash() =>
     r'58e1200804da6c39bdc4f53ec519924e9af148d9';
-
-/// See also [dataSourcePublicRepositories].
-@ProviderFor(dataSourcePublicRepositories)
-final dataSourcePublicRepositoriesProvider =
-    AutoDisposeProvider<DataSourcePublicRepositories>.internal(
-      dataSourcePublicRepositories,
-      name: r'dataSourcePublicRepositoriesProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$dataSourcePublicRepositoriesHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef DataSourcePublicRepositoriesRef =
-    AutoDisposeProviderRef<DataSourcePublicRepositories>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
