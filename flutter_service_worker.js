00a89e0573a618be6fc4d85bf2051eb2ce3498c8
koji-1009 <koji-1009@users.noreply.github.com>
2025-11-25 23:10:26 +0000

    deploy: c3074f7e61e0480aa9a83269ac043c132655a78e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
-"assets/fonts/MaterialIcons-Regular.otf": "b3cc3310d04a452c39cdcc7cbc0a93a5",
+"assets/fonts/MaterialIcons-Regular.otf": "48e4429c77a297c3213545c4f01ad114",
 "assets/NOTICES": "d3b84a0e5ab80733afc184f8cd386070",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
@@ -31,9 +31,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "62cf4554413973eadbec0f44fcab8cef",
+"flutter_bootstrap.js": "e63a133ee7e4357dd4256943232c5981",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
-"main.dart.js": "857b00b50a54d04ccf6f26dd64fcf50f"};
+"main.dart.js": "e4c8fa27cafc8781f8da52e581a43460"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
