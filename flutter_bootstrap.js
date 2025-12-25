dc0e26dc8cbc30c9c870991478aba01f81906613
koji-1009 <koji-1009@users.noreply.github.com>
2025-12-25 03:48:21 +0000

    deploy: cf2c11caaff9c38710a2b18db6a15a5f8799e394

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2348969377"
+    serviceWorkerVersion: "1212952793"
   }
 });
