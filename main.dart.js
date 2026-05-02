ebe5a4c2254b0d9cd8a620af34843f5e2ddb93a4
koji-1009 <koji-1009@users.noreply.github.com>
2026-05-02 06:29:52 +0000

    deploy: eb3782fe5cdcafe1815e2b56b46e87b5592db70d

--- main.dart.js
+++ main.dart.js
@@ -22207,7 +22207,7 @@ $iEE:1}
 A.acQ.prototype={
 $1(a){var s=A.ct().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/42d3d75a56efe1a2e9902f52dc8006099c45d937/":s)+a},
 $S:74}
 A.Hv.prototype={
 gAz(){var s=this.b
