233c767d0c60feb6bd9ecb433f926c8ee05795ad
koji-1009 <koji-1009@users.noreply.github.com>
2026-06-26 23:17:03 +0000

    deploy: da91bf611bede538b92824014bea35922731bd23

--- main.dart.js
+++ main.dart.js
@@ -22393,7 +22393,7 @@ $iF_:1}
 A.adD.prototype={
 $1(a){var s=A.co().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
 $S:88}
 A.HU.prototype={
 gAG(){var s=this.b
